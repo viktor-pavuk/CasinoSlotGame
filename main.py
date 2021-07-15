dee5c789cb71d2be1620a65ef4d80a76aede942c
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2021-07-15 13:13:23 +0300

    new values

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ class Game:
         self.board = np.array([0, 0, 0, 0, 0])
         self.balance = 700
         self.bet = 5
-        self.values = np.array([2, 3, 4, 5, 10, 15, 15])
-        self.symbols = ['10', 'J', 'Q', 'A', 'S', 'R', 'W']
+        self.values = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [4, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [100, 40, 30, 20, 20, 10, 10, 5, 5, 5],
+                                [300, 200, 150, 100, 75, 50, 40, 30, 25, 20],
+                                [500, 400, 300, 250, 200, 160, 140, 120, 110, 100]])
+        self.symbols = ['W', 'P', 'D', 'Z', 'M', 'A', 'K', 'Q', 'J', '1']
 
     def gen_board(self):
         self.board = np.random.randint(0,6,(3,5))
@@ -26,20 +30,20 @@ class Game:
 
     def check_lines(self, line):
         win = 0
-        value = 15
+        value = 0
         for element in line:
-            if element != 6:
-                line = np.where(line==6, element, line)
-                value = self.values[element]
+            if element != 0:
+                line = np.where(line==0, element, line)
+                value = element
                 break
         for i in range(4):
             if line[i] == line[i+1]:
                 win += 1
             else:
                 break
-        if win > 0:
-            print('value', value)
-            print('win', value*win)
+        if self.values[win][value] > 0:
+            print('value', self.symbols[value])
+            print('win', self.values[win][value])
 
     def spin(self):
         self.gen_board()
